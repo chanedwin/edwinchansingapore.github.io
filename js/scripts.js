0fe76c5b3c2c583f4141ebc05fa2fb7682843cca
Edwin Chan <edwinchan@u.nus.edu>
2020-11-12 00:16:04 +0800

    increase scroll speed for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,7 @@
 
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
-        }, 250);
+        }, 100);
 
         // Hide the menu once clicked if mobile
         if ($('header').hasClass('active')) {
@@ -39,7 +39,7 @@
     $('#to-top').click(function() {
         $('html, body').animate({
             scrollTop: 0
-        }, 250);
+        }, 100);
     });
 
     // Scroll to first element
@@ -47,7 +47,7 @@
         var scrollDistance = $('#lead').next().offset().top;
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
-        }, 250);
+        }, 100);
     });
 
     // Create timeline
